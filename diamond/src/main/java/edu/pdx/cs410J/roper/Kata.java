03bd8c3effa5ed19eda6497df51ee27845910f05
Asher Roper <roper@pdx.edu>
2020-07-15 21:01:51 -0700

    half way through paired programming

--- diamond/src/main/java/edu/pdx/cs410J/roper/Kata.java
+++ diamond/src/main/java/edu/pdx/cs410J/roper/Kata.java
@@ -7,10 +7,20 @@ package edu.pdx.cs410J.roper;
  * class (and its tests).
  */
 public class Kata {
-                                                                                    
 
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    char center = args[0].charAt(0);
+    if(center > 'r' || center <= 'a') {
+      System.err.println("command line argument must be a character between a-r");
+      System.exit(1);
+    }
+    char[] toCenter = new char[center-'a'];
+    for(int i = 'a'; i < center; ++i){
+      toCenter[i-'a'] = (char) i;
+    }
+    for (char blah : toCenter) {
+      System.out.println(blah);
+    }
     System.exit(1);
   }
 }
--- diamond/src/test/java/edu/pdx/cs410J/roper/KataTest.java
+++ diamond/src/test/java/edu/pdx/cs410J/roper/KataTest.java
@@ -1,13 +1,29 @@
 package edu.pdx.cs410J.roper;
 
+import edu.pdx.cs410J.InvokeMainTestCase;
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
 
-public class KataTest
+public class KataTest extends InvokeMainTestCase
+
 {
 
   @Test
   public void canInstantiateKataClass() {
     new Kata();
+  }
+
+  @Test
+  public void runMain() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "z");
+    assertThat(result.getTextWrittenToStandardError(), containsString("command line argument must be a character between a-r"));
+  }
+
+  @Test
+  public void completeMain() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(Kata.class, "h");
   }
 
 }
