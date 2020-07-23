67f5a404acf336848e05811431707920b21edf44
Sree-Vandana <sreevandana.sv@gmail.com>
2020-07-22 20:21:56 -0700

    Added a method

--- RPN/src/main/java/edu/pdx/cs410J/ads6/Kata.java
+++ RPN/src/main/java/edu/pdx/cs410J/ads6/Kata.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs410J.ads6;
 
+import java.util.Stack;
+
 /**
  * A class for getting started with a code kata
  *
@@ -11,6 +13,20 @@ public class Kata {
 
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
-    System.exit(1);
+    Stack<String> stack = new Stack<String>();
+    String operators = "+-/*";
+    int num1, num2;
+    // 2 3 + 4 *
+    for(int i=0; i<args.length; i++){
+      if(operators.contains(args[i])){
+        num1 = Integer.parseInt(stack.pop());
+        num2 = Integer.parseInt(stack.pop());
+      }
+      else{
+        stack.push(args[i]);
+      }
+
+
+    }
   }
 }
